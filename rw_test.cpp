f3c133fca62afd8a26996fac3a2365ddf0b6e040
James Ly <jly32@ucmerced.edu>
2018-10-09 14:12:12 -0700

    added rw_test

---
+++ rw_test.cpp
@@ -0,0 +1,18 @@
+#ifdef OLD_HEADER_FILENAME
+#include <iostream.h>
+#else
+#include <iostream>
+#endif
+using std::cout;
+using std::endl;
+#include <string>
+#include "H5Cpp.h"
+using namespace H5;
+const H5std_string FILE_NAME( "assc.h5" );
+const H5std_string DATASET_NAME( "Associations" );
+int main(void){
+    H5File* file = new H5File( FILE_NAME, H5F_ACC_TRUNC );
+    int fillvalue = 0;
+    DSetCreatPropList plist;
+    plist.setFillValue(PredType::NATIVE_INT, &fillvalue);
+}
