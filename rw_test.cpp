ae168139986614cfd7237a1a2dd6f4193c55ba88
James Ly <jly32@ucmerced.edu>
2018-10-18 22:28:18 -0700

    some rw_tests

--- rw_test.cpp
+++ rw_test.cpp
@@ -13,6 +13,8 @@ const H5std_string DATASET_NAME( "Associations" );
 int main(void){
     H5File* file = new H5File( FILE_NAME, H5F_ACC_TRUNC );
     int fillvalue = 0;
-    DSetCreatPropList plist;
+    DSetCreatPropList plist;    
     plist.setFillValue(PredType::NATIVE_INT, &fillvalue);
+    hsize_t fdim[] = {5, 200}; // dim sizes of ds (on disk)
+    DataSpace fspace( FSPACE_RANK, fdim );
 }
